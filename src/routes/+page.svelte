dbe1d2bd8e80542fc346783ce2bff37869985e47
AndryOre <andryp_@hotmail.com>
2025-02-28 15:37:03 -0400

    feat(ui): ✨ add shadcn
    - downgrade tailwindcss to v3 for compatibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<h1 class="text-3xl font-bold underline text-orange-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me</Button>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
